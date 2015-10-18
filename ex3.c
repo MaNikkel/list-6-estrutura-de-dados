c33cef37c996b84b02325d0fe6c232779e5327c2
Mathias <mathiasnikkel@gmail.com>
2015-10-18 16:24:14 -0200

    inclusão do strcpy()

--- ex3.c
+++ ex3.c
@@ -15,13 +15,31 @@ int len(char *str)
 	}
 }
 
+void cpy(char *str1, char *str2)
+{
+	int i, strCopied;
+	i = 0;
+	strCopied = 0;
+	while(strCopied == 0)
+	{
+		str1[i] = str2[i];
+		if(str2[i] == '\0')
+			strCopied = 1;
+		i++;
+	}
+	
+}
 
 int main()
 {
 	char string1[30], string2[30];
 	printf("digite a string 1: ");
 	gets(string1);
-	printf("tamanho string 1: %d", len(string1));
+	printf("tamanho string 1: %d\n", len(string1));
+	cpy(string2, string1);
+	printf("string 1 = %s\nstring 2 = %s\n", string1, string2);
+	
+	
 	return 0;
 	
 }
