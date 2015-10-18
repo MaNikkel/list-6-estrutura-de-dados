5c60c541961ab00602436515637a768eff8d8c4d
Mathias <mathiasnikkel@gmail.com>
2015-10-18 16:17:06 -0200

    inclusão do strlen()

--- ex3.c
+++ ex3.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int len(char *str)
+{
+	int i;
+	i = 0;
+	while(i > -1)
+	{
+		if(str[i] == '\0')
+		{
+			return i;
+		}
+		i++;
+	}
+}
 
 
 int main()
 {
 	char string1[30], string2[30];
 	printf("digite a string 1: ");
-	gets(string1)
+	gets(string1);
+	printf("tamanho string 1: %d", len(string1));
+	return 0;
+	
 }
