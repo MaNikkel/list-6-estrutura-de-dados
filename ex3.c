f039b0c4e445f5c2783b4c2ca1e249a9b7c5b3c4
Mathias <mathiasnikkel@gmail.com>
2015-10-18 16:33:10 -0200

    inclusão do str upr

--- ex3.c
+++ ex3.c
@@ -30,6 +30,19 @@ void cpy(char *str1, char *str2)
 	
 }
 
+void upr(char *str)
+{
+	int i;
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if(str[i] > 90)
+			str[i] = str[i] - 32;
+		i++;
+	}
+}
+
+
 int main()
 {
 	char string1[30], string2[30];
@@ -38,6 +51,8 @@ int main()
 	printf("tamanho string 1: %d\n", len(string1));
 	cpy(string2, string1);
 	printf("string 1 = %s\nstring 2 = %s\n", string1, string2);
+	upr(string1);
+	printf("string 1 maiuscula = %s\n", string1);
 	
 	
 	return 0;
